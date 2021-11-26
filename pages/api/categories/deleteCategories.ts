b4fecb46326c505f7debb03497e50a494f0ab6a0
Christian Pannwitz <ch.von.pa@gmail.com>
2021-11-26 08:53:40 +0100

    added crud category apis

--- pages/api/categories/deleteCategories.ts
+++ pages/api/categories/deleteCategories.ts
@@ -1,33 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next"
-import { PrismaClient, Prisma } from ".prisma/client"
-
-import { TransactionWithCategories } from "../../types/types"
+import { PrismaClient, Prisma, Category } from ".prisma/client"
 
 const prisma = new PrismaClient()
 
 type ResponseData = {
   error?: any
-  data?: TransactionWithCategories[]
+  data?: Category[]
 }
 
-export default async function addTransactions(
+export default async function deleteCategories(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
   if (req.method === "POST") {
-    const bodydata = req.body as Prisma.TransactionCreateInput[]
+    const bodydata = req.body as Prisma.CategoryWhereUniqueInput[]
 
     try {
       const insertedData = await prisma.$transaction(
         bodydata.map(data =>
-          prisma.transaction.upsert({
+          prisma.category.delete({
             where: {
-              identifier: data.identifier ?? undefined,
+              id: data.id,
             },
-            update: data,
-            create: data,
-            include: { categories: true },
           })
         )
       )
